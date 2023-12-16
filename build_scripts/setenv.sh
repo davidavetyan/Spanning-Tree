855ff93ec1e54d13372c6e68a8bd6224b8862edc
David Avetyan <davidavetyan15@gmail.com>
2023-12-16 19:21:29 +0400

    Update environment paths in linux build script

--- build_scripts/setenv.sh
+++ build_scripts/setenv.sh
@@ -5,12 +5,12 @@ export APP_NAME=SpanTree
 export APP_ROOT=$CURRENT_DIR/..
 
 if [ -z ${QTDIR} ] ; then
-    export QTDIR=/home/davetyan/depot/Qt/5.15.2/wasm_32
+    export QTDIR=$HOME/depot/Qt/5.15.2/wasm_32
 fi
 
 export QMAKE=$QTDIR/bin/qmake
 export MAKE=make
 
-export EMSDK_ENV=/home/davetyan/depot/emsdk/emsdk_env.sh
+export EMSDK_ENV=$HOME/depot/emsdk/emsdk_env.sh
 
 source $EMSDK_ENV
